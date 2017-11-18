02942ccf02649e6fb809ab18cb530c44de6eb491
Jan Mrhar <jan.jn3@gmail.com>
2017-11-18 12:04:49 +0100

    result page done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import jinja2
 import webapp2
+import library.models
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
