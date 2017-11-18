9cfef5d3ade6dd8c7b7a2997df1bd6e1d9965319
Jan Mrhar <jan.jn3@gmail.com>
2017-11-18 13:20:00 +0100

    api parsing done

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import os
 import jinja2
 import webapp2
 import library.models
+import urllib2
+import urllib
+import json
+import math
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+url = "http://www.omdbapi.com/?apikey=b7c1146a&s=%s&page=%s"
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -28,7 +34,39 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+
+        query = self.request.get("q")
+        page = self.request.get("p")
+
+        query = query.strip()
+
+        if len(page) <= 0:
+            page = 1
+
+        page = int(page)
+        if page < 1:
+            page = 1
+
+        results = {}
+        foundRows = 0
+        pages = 0
+        if len(query) > 0:
+            query = urllib.quote_plus(query)
+            url = "http://www.omdbapi.com/?apikey=b7c1146a&s=%s&page=%s"
+            url = url % (query, page)
+            content = urllib2.urlopen(url=url).read()
+            obj = json.loads(content)
+            results = obj["Search"]
+            foundRows = int(obj["totalResults"])
+            pages = int(math.ceil(float(foundRows) / 10))
+
+        return self.render_template("hello.html", {
+            "results": results,
+            "foundRows": foundRows,
+            "page": page,
+            "pages": pages,
+            "query": query
+        })
 
 
 app = webapp2.WSGIApplication([
